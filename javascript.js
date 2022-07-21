b0038d54bd1273dd8554732b27425f0969e71232
Pocketts <jakehicks7588@yahoo.com>
2022-07-20 21:11:02 -0500

    Add button onclick fuctionprint comChoice

--- javascript.js
+++ javascript.js
@@ -2,5 +2,8 @@ const choices = ["Rock" , "Paper" , "Scissors"];
 
 const getComputerChoice = Math.floor(Math.random() * choices.length);
 
-console.log(choices[getComputerChoice]);
+const comChoice = (choices[getComputerChoice]);
 
+function round() {
+    document.write(comChoice);
+}
