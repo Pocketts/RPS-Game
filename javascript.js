02633802bfd38f580d678d5cce43cd258d9ae2d8
Pocketts <jakehicks7588@yahoo.com>
2022-07-23 13:59:38 -0500

    Added Win and Lose conditions to on click funtion.

--- javascript.js
+++ javascript.js
@@ -1,25 +1,40 @@
-//This randomizes the computers choice//
-const choices = ["Rock" , "Paper" , "Scissors"];
+
+const R = "ROCK"
+const P = "PAPER"
+const S = "SCISSORS"
+
+function round() {
+
+    //This randomizes the computers choice//
+const choices = ["ROCK" , "PAPER" , "SCISSORS"];
 
 const getComputerChoice = Math.floor(Math.random() * choices.length);
 
 const comChoice = (choices[getComputerChoice]);
-//End//
-const rock = "ROCK"
-const paper = "PAPER"
-const Scissors = "SCISSORS"
+   
 
-function round() {
-    
+   //Captures and converts player input// 
     let playerChoice = document.getElementById("pChoice").value;
     playerChoice = playerChoice.toUpperCase();
+  
 
-    if (playerChoice == rock) {
+    console.log(playerChoice)
+    console.log(comChoice)
+
+    if (playerChoice == comChoice) {
         
-        document.getElementById("result").innerHTML = comChoice + " " + playerChoice;
+        document.getElementById("result").innerHTML = "Draw no points scored";
 
     }
+    else if (playerChoice == R && comChoice == S || playerChoice == P && comChoice == R || playerChoice == S && comChoice == P)  {
+        document.getElementById("result").innerHTML = "You Win This Round"; 
+    }
+
+    else if (playerChoice == R && comChoice == P || playerChoice == P && comChoice == S || playerChoice == S && comChoice == R) {
+        document.getElementById("result").innerHTML = "You Lose This Round";
+    }
+
     else {
-        document.getElementById("result").innerHTML = comChoice + " nope"; 
+        document.getElementById("result").innerHTML = "Not a Valid Choice";
     }
 }
