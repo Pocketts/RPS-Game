67e2f1e6e09356147edfb9883cc47464d826ca43
Pocketts <jakehicks7588@yahoo.com>
2022-07-23 13:18:07 -0500

    Comments and player input conversion

--- javascript.js
+++ javascript.js
@@ -1,13 +1,25 @@
+//This randomizes the computers choice//
 const choices = ["Rock" , "Paper" , "Scissors"];
 
 const getComputerChoice = Math.floor(Math.random() * choices.length);
 
 const comChoice = (choices[getComputerChoice]);
-
-
+//End//
+const rock = "ROCK"
+const paper = "PAPER"
+const Scissors = "SCISSORS"
 
 function round() {
+    
+    let playerChoice = document.getElementById("pChoice").value;
+    playerChoice = playerChoice.toUpperCase();
 
-    const pChoice = document.getElementById("pChoice").value;
-    document.getElementById("result").innerHTML = comChoice + " " + pChoice;
+    if (playerChoice == rock) {
+        
+        document.getElementById("result").innerHTML = comChoice + " " + playerChoice;
+
+    }
+    else {
+        document.getElementById("result").innerHTML = comChoice + " nope"; 
+    }
 }
