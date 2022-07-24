8c97db92cee9b4d78137605fb132df5a22685015
Pocketts <jakehicks7588@yahoo.com>
2022-07-24 08:27:48 -0500

    Added score tracking

--- javascript.js
+++ javascript.js
@@ -2,6 +2,9 @@
 const R = "ROCK"
 const P = "PAPER"
 const S = "SCISSORS"
+let playerScore = 0
+let computerScore = 0
+
 
 function round() {
 
@@ -21,20 +24,34 @@ const comChoice = (choices[getComputerChoice]);
     console.log(playerChoice)
     console.log(comChoice)
 
-    if (playerChoice == comChoice) {
+    
+
+    
+
+      if (playerChoice == comChoice) {
         
-        document.getElementById("result").innerHTML = "Draw no points scored";
+          document.getElementById("result").innerHTML = "Draw no points scored";
 
-    }
-    else if (playerChoice == R && comChoice == S || playerChoice == P && comChoice == R || playerChoice == S && comChoice == P)  {
-        document.getElementById("result").innerHTML = "You Win This Round"; 
-    }
+      }
+      else if (playerChoice == R && comChoice == S || playerChoice == P && comChoice == R || playerChoice == S && comChoice == P)  {
+          document.getElementById("result").innerHTML = "You Win This Round"; 
+          playerScore = playerScore + 1;
+      }
 
-    else if (playerChoice == R && comChoice == P || playerChoice == P && comChoice == S || playerChoice == S && comChoice == R) {
-        document.getElementById("result").innerHTML = "You Lose This Round";
-    }
+      else if (playerChoice == R && comChoice == P || playerChoice == P && comChoice == S || playerChoice == S && comChoice == R) {
+          document.getElementById("result").innerHTML = "You Lose This Round";
+          computerScore = computerScore + 1;
+        }
 
-    else {
-        document.getElementById("result").innerHTML = "Not a Valid Choice";
-    }
+     else {
+            document.getElementById("result").innerHTML = "Not a Valid Choice";
+        }
+
+      // playerScore = (playerScore).toString;
+       //computerScore = (computerScore).toString;
+
+        document.getElementById("pScore").innerHTML = "Player Score " + playerScore;
+        document.getElementById("cScore").innerHTML = "Computer Score " + computerScore;
+    
 }
+
